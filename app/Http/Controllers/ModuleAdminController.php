6001fdd1d3231f546848153033765cf40cb30b1a
Quentin <hausser.quentin@gmail.com>
2015-02-16 16:01:58 +0100

    Module edition

--- app/Http/Controllers/ModuleAdminController.php
+++ app/Http/Controllers/ModuleAdminController.php
@@ -2,7 +2,10 @@
 
 namespace LearnerApi\Http\Controllers;
 
+use LearnerApi\Http\Middleware\ModuleUpdateRequest;
 use LearnerApi\Module;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 
 class ModuleAdminController extends AdminController {
 
@@ -11,4 +14,32 @@ class ModuleAdminController extends AdminController {
 		return view('modules.module')->with('modules', Module::all());
 	}
 
+	public function getEditModule($id)
+	{
+		return view('modules.edit')->with('module', Module::find($id));
+	}
+
+	public function postUpdateModule(ModuleUpdateRequest $request)
+	{
+		$update = $request->all();
+
+		$module = Module::find($update['module-id']);
+		$module->description = $update['module-description'];
+		$module->title = $update['module-title'];
+		if ($request->hasFile('module-picture'))
+		{
+			if ($module->img != null)
+			{
+				$filename = explode('/', $module->img);
+				if (File::exists('resources/modules/' . end($filename)))
+					File::delete('resources/modules/' . end($filename));
+			}
+			$filename = Str::random($lenght = 30) . '.' . $update['module-picture']->getClientOriginalExtension();
+			$update['module-picture']->move('resources/modules', $filename);
+			$module->img = asset('resources/modules/' . $filename);
+		}
+		$module->save();
+
+		return view('modules.edit')->with('module', Module::find($request['module-id']))->withErrors(['success' => 'Module updated with success.']);
+	}
 }
