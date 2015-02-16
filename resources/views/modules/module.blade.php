f41ea3c20f4c3910d56dd91fc73c804fca2f2968
Quentin <hausser.quentin@gmail.com>
2015-02-16 14:49:13 +0100

    no more Vendor folder

--- resources/views/modules/module.blade.php
+++ resources/views/modules/module.blade.php
@@ -4,8 +4,10 @@
     <div class="container">
         @foreach($modules as $module)
             <div class="panel panel-default">
-                <div class="panel-body">
+                <div class="panel-body" style="text-align: center;">
                     {!! $module->title !!}
+                    {!! HTML::image($module->img, null, (['class' => 'img-responsive img-rounded', 'style' => 'max-height: 300px; margin-right: auto; margin-left: auto;'])) !!}
+                    <a style="text-decoration: none;" class="btn btn-success" href="">Editer</a>
                 </div>
             </div>
         @endforeach
