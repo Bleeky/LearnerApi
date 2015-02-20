21c304a7149fbe3ccff221a1a05334ef896e8479
Quentin <hausser.quentin@gmail.com>
2015-02-20 16:38:07 +0100

    Mistake correction :sorry

--- resources/views/diapos/diapo.blade.php
+++ resources/views/diapos/diapo.blade.php
@@ -4,10 +4,10 @@
     <div class="container">
         <a style="text-decoration: none; margin-top: 13px; margin-bottom: 13px;" class="btn btn-primary"
            href="{{ URL::action('ModuleAdminController@getIndex') }}">Retour</a>
-    @if (!empty($diapos))
+        @if (!empty($diapos))
+            <a style="text-decoration: none;" class="btn btn-success"
+               href="{{ URL::action('DiapoAdminController@getInsertDiapo', 0) }}">Insérer une diapo</a>
             @foreach($diapos as $elem)
-                <a style="text-decoration: none; margin-top: 13px; margin-bottom: 13px;" class="btn btn-success"
-                   href="{{ URL::action('DiapoAdminController@getInsertDiapo', [$elem['id']]) }}">Insérer une diapo</a>
                 @if ($elem[0]->type == "1")
                     @include('diapos.type.type1')
                 @elseif($elem[0]->type == "2")
@@ -25,6 +25,8 @@
                 @elseif($elem[0]->type == "8")
                     @include('diapos.type.type8')
                 @endif
+                <a style="text-decoration: none; margin-top: 13px; margin-bottom: 13px;" class="btn btn-success"
+                   href="{{ URL::action('DiapoAdminController@getInsertDiapo', [$elem['id']]) }}">Insérer une diapo</a>
             @endforeach
         @else
             <div class="panel panel-default">
